e942710d2fe8de72b12a85b45e301067b328d55e
chrysanthusobinna <chrysanthusobinna@gmail.com>
2024-07-11 06:56:19 +0100

    Add environment variable support and debug for SQL Server connection issues

--- config.php
+++ config.php
@@ -1,21 +1,36 @@
 <?php
-session_start();   
 
-// db configuration variables
-$serverName    =    "SANTHUS\SQLEXPRESS";
-$database      =    "survery-form";
-$uid           =    "";
-$pwd           =    "";
+function loadEnv($file) {
+    if (file_exists($file)) {
+        $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            // Remove comments and trim whitespace
+            $line = trim(preg_replace('/\s*#.*$/', '', $line));
+            if ($line && strpos($line, '=') !== false) {
+                list($name, $value) = explode('=', $line, 2);
+                putenv(trim($name) . '=' . trim($value));
+            }
+        }
+    }
+}
 
-// Connect to the local server using Windows Authentication.
+loadEnv(__DIR__ . '/.env');
+
+$serverName = getenv('DB_SERVER_NAME');
+$database = getenv('DB_DATABASE');
+$uid = getenv('DB_UID');
+$pwd = getenv('DB_PWD');
+
+//database connection
 $connectionInfo = array("Database" => $database, "Uid" => $uid, "PWD" => $pwd);
-$conn           = sqlsrv_connect($serverName, $connectionInfo);
-
- 
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 if (!$conn) {
     die(print_r(sqlsrv_errors(), true));
 }
+ 
+session_start();   
+
   
 
 ?>
